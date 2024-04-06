f5b159151f292a1459a8128ce1b42e548169c865
Diego Caldas Salengue <dcsalengue@me.com>
2024-04-06 11:53:40 -0300

    03. useLocation e state

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,7 +1,7 @@
 import styles from './Prato.module.scss';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 export default function Prato() {
-  console.log(useParams());
+  console.log(useLocation());
   return (
     
     <div>
